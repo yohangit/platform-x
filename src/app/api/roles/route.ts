518f78800282448daf9838bba7dcdd51aa3565c4
yohangit <yohanlw@gmail.com>
2023-10-06 10:28:54 +0530

    Changed the Ids

--- src/app/api/roles/route.ts
+++ src/app/api/roles/route.ts
@@ -1,6 +1,6 @@
 import {NextResponse} from 'next/server'
 
 export async function GET(request: Request) {
-  return NextResponse.json([{"id":1, "name":"Developer"},{"id":2, "name":"UX Engineer"},{"id":1, "name":"Business Analyst"}])
+  return NextResponse.json([{"id":1, "name":"Developer"},{"id":2, "name":"UX Engineer"},{"id":3, "name":"Business Analyst"}])
   }
   
--- src/app/api/technologies/route.ts
+++ src/app/api/technologies/route.ts
@@ -1,6 +1,6 @@
 import {NextResponse} from 'next/server'
 
 export async function GET(request: Request) {
-  return NextResponse.json([{"id":1, "name":"Next.js"},{"id":2, "name":"Python"},{"id":1, "name":"AWS"}])
+  return NextResponse.json([{"id":1, "name":"Next.js"},{"id":2, "name":"Python"},{"id":3, "name":"AWS"}])
   }
   
